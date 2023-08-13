370edd82abdf1ec8c44ac6b2fef3963fd327fb22
Ulyana Drozdova <uad474@gmail.com>
2023-08-13 20:19:02 +0200

    Connect to WIFI

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,31 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char* ssid = "InnoWarsaw";
+const char* password = "Wars299!";
+
+void ConnectedToAP_Handler(WiFiEvent_t wifi_event, WiFiEventInfo_t wifi_info);
+void GotIP_Handler(WiFiEvent_t wifi_event, WiFiEventInfo_t wifi_info);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.onEvent(ConnectedToAP_Handler, ARDUINO_EVENT_WIFI_STA_CONNECTED);
+  WiFi.onEvent(GotIP_Handler, ARDUINO_EVENT_WIFI_STA_GOT_IP);
+  WiFi.begin(ssid, password);
+  Serial.println("\nConnecting to WiFi Network ..");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ 
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void ConnectedToAP_Handler(WiFiEvent_t wifi_event, WiFiEventInfo_t wifi_info) {
+  Serial.println("Connected To The WiFi Network");
+}
+
+void GotIP_Handler(WiFiEvent_t wifi_event, WiFiEventInfo_t wifi_info) {
+  Serial.print("Local ESP32 IP: ");
+  Serial.println(WiFi.localIP());
 }
