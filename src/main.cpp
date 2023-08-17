7ed35f3438ce7a7d098af282290eff32e68c4aa1
Ulyana Drozdova <uad474@gmail.com>
2023-08-17 11:53:54 +0200

    Getting hour using NTP library, configured my timezone and initialized pin 23 on output

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,21 @@
 #include <Arduino.h>
+#include <NTPClient.h>
 #include <WiFi.h>
+#include <WiFiUdp.h>
+
+#define UTC_OFFSET_IN_SECONDS 3600         // offset from greenwich time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", UTC_OFFSET_IN_SECONDS);
 
 const char* ssid = "InnoWarsaw";
-const char* password = "Wars299!";
+const char* password = "Wise299!";
+
+int hour;
 
 void ConnectedToAP_Handler(WiFiEvent_t wifi_event, WiFiEventInfo_t wifi_info);
 void GotIP_Handler(WiFiEvent_t wifi_event, WiFiEventInfo_t wifi_info);
+
+const int ledPin = 23;
 
 void setup() {
   Serial.begin(115200);
@@ -14,15 +24,26 @@ void setup() {
   WiFi.onEvent(ConnectedToAP_Handler, ARDUINO_EVENT_WIFI_STA_CONNECTED);
   WiFi.onEvent(GotIP_Handler, ARDUINO_EVENT_WIFI_STA_GOT_IP);
   WiFi.begin(ssid, password);
+  timeClient.begin();
+  timeClient.setTimeOffset(10800);
   Serial.println("\nConnecting to WiFi Network ..");
+
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
- 
+  timeClient.update();
+
+  hour = timeClient.getHours();
+  Serial.print(hour);
+
+  delay(1000);
+  
 }
 
 void ConnectedToAP_Handler(WiFiEvent_t wifi_event, WiFiEventInfo_t wifi_info) {
   Serial.println("Connected To The WiFi Network");
+
 }
 
 void GotIP_Handler(WiFiEvent_t wifi_event, WiFiEventInfo_t wifi_info) {
